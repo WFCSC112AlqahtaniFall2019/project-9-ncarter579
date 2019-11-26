fe06e76fb6dfd814b8e7e5cea537120d5cd3c2a8
Nathan Carter <cartnl18@wfu.edu>
2019-11-25 21:48:40 -0500

    implemeted a new merge sort, all of the function calls for the different vectors, and the timers for each function call

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ template
         swapped=false;
         for (j = 0; j < size-i-1; j++){
             if (arr[j] > arr[j+1]) {
-                swap(&arr[j], &arr[j + 1]);
+                swap(arr[j], arr[j + 1]);
                 swapped = true;
             }
             //if there wasn't any swap in the inner loop, stop
@@ -46,7 +46,7 @@ template
 
 //selection sort from zybooks 15.6
 template
-<typename T>void SelectionSort(int numbers[], int numbersSize) {
+<typename T>void SelectionSort(vector <T>& arr, int numbersSize) {
     int i;
     int j;
     int indexSmallest;
@@ -55,82 +55,63 @@ template
         // Find index of smallest remaining element
         indexSmallest = i;
         for (j = i + 1; j < numbersSize; ++j) {
-            if ( numbers[j] < numbers[indexSmallest] ) {
+            if ( arr[j] < arr[indexSmallest] ) {
                 indexSmallest = j;
             }
         }
         // Swap numbers[i] and numbers[indexSmallest]
-        temp = numbers[i];
-        numbers[i] = numbers[indexSmallest];
-        numbers[indexSmallest] = temp;
+        swap(arr[i],arr[indexSmallest]);
     }
 }
 
 //mergeSort from zybooks 15.9
 template
-<typename T>void Merge(T numbers[], int i, int j, int k) {
-    int mergedSize;                            // Size of merged partition
-    int mergePos;                              // Position to insert merged number
-    int leftPos;                               // Position of elements in left partition
-    int rightPos;                              // Position of elements in right partition
-    int* mergedNumbers = nullptr;
-
-    mergePos = 0;
-    mergedSize = k - i + 1;
-    leftPos = i;                               // Initialize left partition position
-    rightPos = j + 1;                          // Initialize right partition position
-    mergedNumbers = new int[mergedSize];       // Dynamically allocates temporary array
-    // for merged numbers
-
-    // Add smallest element from left or right partition to merged numbers
-    while (leftPos <= j && rightPos <= k) {
-        if (numbers[leftPos] < numbers[rightPos]) {
-            mergedNumbers[mergePos] = numbers[leftPos];
-            ++leftPos;
+<typename T>void Merge(vector<T>& v,vector<T>& t, int left, int middle,  int right){
+    int temp = left;                                //setting up a temp value that is initialised to the leftmost value in the vector
+    int tempL = left;                           //setting up a temp value that will be used as the left side of each vector we're working with
+    int tempR = middle + 1;                         //setting up a temp value that will be used as the right side of each vector we're working with
+    while(tempL <= middle && tempR <= right ){      //will correct one side of the vector first
+        if(v.at(tempL) > v.at(tempR)){          //if the left value is greater then the right value do this
+            t.at(temp) = v.at(tempR);           //setting the temporary vector with the correct number
+            temp = temp + 1;                    //iterating the variables used
+            tempR = tempR + 1;
         }
-        else {
-            mergedNumbers[mergePos] = numbers[rightPos];
-            ++rightPos;
-
+        else{
+            t.at(temp) = v.at(tempL);       //setting the temporary vector with the correct number
+            temp = temp + 1;            //iterating the variables used
+            tempL = tempL + 1;
         }
-        ++mergePos;
     }
-
-    // If left partition is not empty, add remaining elements to merged numbers
-    while (leftPos <= j) {
-        mergedNumbers[mergePos] = numbers[leftPos];
-        ++leftPos;
-        ++mergePos;
+    while(tempR <= right){          //checking the right side
+        t.at(temp) = v.at(tempR);       //setting the temporary vector with the correct number
+        temp = temp + 1;            //iterating the variables used
+        tempR = tempR + 1;
     }
-
-    // If right partition is not empty, add remaining elements to merged numbers
-    while (rightPos <= k) {
-        mergedNumbers[mergePos] = numbers[rightPos];
-        ++rightPos;
-        ++mergePos;
+    while(tempL <= middle){         //checking the left side
+        t.at(temp) = v.at(tempL);       //setting the temporary vector with the correct number
+        temp = temp + 1;            //iterating the variables used
+        tempL = tempL + 1;
     }
-
-    // Copy merge number back to numbers
-    for (mergePos = 0; mergePos < mergedSize; ++mergePos) {
-        numbers[i + mergePos] = mergedNumbers[mergePos];
+    for(int i = left; i <= right; i++ ){        //resetting vector v with the correct values that had been stored in vector t
+        v.at(i) = t.at(i);
     }
-    delete[] mergedNumbers;
 }
 
-void MergeSort(int numbers[], int i, int k) {
-    int j;
+template
+<typename T>void MergeSort(vector <T>& numbers,vector<T>& t,int left,int right) {  //calling mergeSort with the real vectors this time
+    int center;             //setting up a center variable so that we can find the center of each vector were working in every time we call the function
+    if(right <= left){      //checking to see if its in the correct order
+        return;             //if its in the right order we can return because no further action is needed
+    }
+    else{
+        center = (left + right)/2;                          //finding center of the vector
+        MergeSort(numbers,t,left,center);                 //calls mergeSort recursively the first time to get the left side of the split
+        MergeSort(numbers,t,center + 1,right);        //calls mergeSort recursively the second time to get the right side
+        Merge(numbers,t,left,center,right);     //calls Merge to combine the vectors after they are broken down
 
-    if (i < k) {
-        j = (i + k) / 2;  // Find the midpoint in the partition
-
-        // Recursively sort left and right partitions
-        MergeSort(numbers, i, j);
-        MergeSort(numbers, j + 1, k);
-
-        // Merge left and right partition in sorted order
-        Merge(numbers, i, j, k);
     }
 }
+
 
 
 
@@ -169,7 +150,7 @@ int main() {
 
     cout << "Reading ../dataset.csv…" << endl;
 
-    for(int i = 0; i < 100500; i++){             //for the length of the data set
+    for(int i = 0; i < 149999; i++){             //for the length of the data set
         //while(!inputs.eof()){
         getline(inputs, loc, ',');           //getting the inputs from the data set and storing them in the strings
         getline(inputs, pres, ',');
@@ -186,7 +167,7 @@ int main() {
     }
 
     srand(time(0));
-    for(int i = 0; i < 100500; i++){
+    for(int i = 0; i < 149999; i++){
         int r;
         r = rand() % 10000;
 
@@ -197,6 +178,72 @@ int main() {
         intV_M.emplace_back(r);
 
     }
+
+    clock_t start_dataBubbleSort1 = clock();
+    bubbleSort(dataV_B,149999);
+    clock_t end_dataBubbleSort1 = clock();
+    clock_t start_dataBubbleSort2 = clock();
+    bubbleSort(dataV_B,149999);
+    clock_t end_dataBubbleSort2 = clock();
+
+    clock_t start_intBubbleSort1 = clock();
+    bubbleSort(intV_B,149999);
+    clock_t end_intBubbleSort1 = clock();
+    clock_t start_intBubbleSort2 = clock();
+    bubbleSort(intV_B,149999);
+    clock_t end_intBubbleSort2 = clock();
+
+    clock_t start_dataMinSort1 = clock();
+    MinSort(dataV_M,149999);
+    clock_t end_dataMinSort1 = clock();
+    clock_t start_dataMinSort2 = clock();
+    MinSort(dataV_M,149999);
+    clock_t end_dataMinSort2 = clock();
+
+    clock_t start_intMinSort1 = clock();
+    MinSort(intV_M,149999);
+    clock_t end_intMinSort1 = clock();
+    clock_t start_intMinSort2 = clock();
+    MinSort(intV_M,149999);
+    clock_t end_intMinSort2 = clock();
+
+    clock_t start_dataSelectionSort1 = clock();
+    SelectionSort(dataV_S,149999);
+    clock_t end_dataSelectionSort1 = clock();
+    clock_t start_dataSelectionSort2 = clock();
+    SelectionSort(dataV_S,149999);
+    clock_t end_dataSelectionSort2 = clock();
+
+    clock_t start_intSelectionSort1 = clock();
+    SelectionSort(intV_S,149999);
+    clock_t end_intSelectionSort1 = clock();
+    clock_t start_intSelectionSort2 = clock();
+    SelectionSort(intV_S,149999);
+    clock_t end_intSelectionSort2 = clock();
+
+    vector<Data> t(149999);
+    clock_t start_dataMergeSort1 = clock();
+    MergeSort(dataV_Q,t, 0, 149998);
+    clock_t end_dataMergeSort1 = clock();
+    clock_t start_dataMergeSort2 = clock();
+    MergeSort(dataV_Q,t, 0, 149998);
+    clock_t end_dataMergeSort2 = clock();
+
+    vector<int> a(149999);
+    clock_t start_intMergeSort1 = clock();
+    MergeSort(intV_Q,a, 0, 149998);
+    clock_t end_intMergeSort1 = clock();
+    clock_t start_intMergeSort2 = clock();
+    MergeSort(intV_Q,a, 0, 149998);
+    clock_t end_intMergeSort2 = clock();
+
+
+
+
+
+
+
+
 
 
 
